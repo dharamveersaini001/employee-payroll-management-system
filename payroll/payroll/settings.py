9326b03a5b25ffc363365c34aef02bc779537c5c
Dharamveer <sainidharamveer787@gmail.com>
2026-09-18 19:42:48 +0530

    Prepare Django project for deployment

--- payroll/payroll/settings.py
+++ payroll/payroll/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t@*7-5vvogog@fhuveyu%w8yq2tqw9*kaz1@le35o3^)=j7-m6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 # Email
